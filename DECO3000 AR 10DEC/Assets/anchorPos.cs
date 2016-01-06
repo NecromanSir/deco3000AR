9f62aafc340a5719bc2c776a2f170bbcc9791c00
NecromanSir <masonajames@gmail.com>
2016-01-06 10:50:42 +1000

    Plane now child of meta cube
    needs to have gravity and other items adjusted to match angles

---
+++ DECO3000 AR 10DEC/Assets/anchorPos.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anchorPos : MonoBehaviour {
+
+    public Transform theAnchor;
+
+    
+	// Use this for initialization
+	void Start () {
+        transform.parent = theAnchor.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = theAnchor.transform.position;
+    }
+}
