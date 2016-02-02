cb4f7534e234a1ab8085be81817a08d922764188
NecromanSir <masonajames@gmail.com>
2016-02-02 23:23:57 +1000

    Fixed Player Collision and Imported Door
    Door animation triggers need fixing

---
+++ DECO3000 AR 10DEC/Assets/SciFi_Door/Script/door.js
@@ -0,0 +1,15 @@
+﻿//using UnityEngine;
+//using System.Collections;
+
+//#pragma strict
+
+
+//function OnTriggerEnter (obj : Collider) {
+//	var thedoor = gameObject.FindWithTag("SF_Door");
+//	thedoor.animation.Play("open");
+//}
+
+//function OnTriggerExit (obj : Collider) {
+//	var thedoor = gameObject.FindWithTag("SF_Door");
+//	thedoor.animation.Play("close");
+//}
---
+++ DECO3000 AR 10DEC/Assets/SciFi_Door/Script/door_jam.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door_jam : MonoBehaviour {
+
+    private Transform ground;
+    private Transform target;
+    private Animation animator;
+    private bool playerPresent = false;
+
+    // Use this for initialization
+    void Start () {
+        ground = GameObject.Find("Plane").transform;
+        transform.parent = ground.transform;
+
+        animator = GetComponent<Animation>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (GameObject.FindGameObjectsWithTag("playerPrefab").Length > 0)
+        {
+            playerPresent = true;
+            target = GameObject.FindWithTag("playerPrefab").transform;
+        }
+        else
+        {
+            target = GameObject.FindWithTag("CubeZom").transform;
+            playerPresent = false;
+        }
+
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+
+        if (distance < 1.0f)
+        {
+            //animator.Play("open");
+            //animator.SetBool("keyNear", true);
+        } else
+        {
+            animator.Play("close");
+            //animator.SetBool("keyNear", false);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "playerPrefab")
+        {
+            animator.Play("open");
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "playerPrefab")
+        {
+            animator.Play("close");
+        }
+    }
+}
