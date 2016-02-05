d4fa610ce22ade2ec433182c897edeaf51816d70
NecromanSir <masonajames@gmail.com>
2016-02-06 00:52:18 +1000

    Added Necromancer Targeting and Movement
    also fixed zombie crawler collider

---
+++ DECO3000 AR 10DEC/Assets/necromancerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class necromancerScript : MonoBehaviour {
+
+    private Transform ground;
+    private Transform target;
+    private bool playerPresent = false;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        ground = GameObject.Find("Plane").transform;
+        transform.parent = ground.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(GameObject.FindGameObjectsWithTag("playerPrefab").Length > 0) {
+            playerPresent = true;
+            target = GameObject.FindWithTag("playerPrefab").transform;
+        } else
+        {
+            target = GameObject.FindWithTag("CubeZom").transform;
+            playerPresent = false;
+        }
+        float step = speed * Time.deltaTime;
+        var targetPos = target.position;
+        transform.LookAt(targetPos, target.up);
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
+
+        //if (transform.eulerAngles.z != 0 || transform.eulerAngles.x != 0)
+        //{
+        //    transform.rotation = Quaternion.Euler(0, 0, 0);
+        //    transform.LookAt(targetPos, target.up);
+        //}
+    }
+}
